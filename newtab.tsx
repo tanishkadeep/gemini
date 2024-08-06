4500722a22bae11206a369516f1d9a94e2074e91
Tanishka <tanishkadeep09@gmail.com>
2024-08-06 17:11:47 +0530

    Initial commit

---
+++ newtab.tsx
@@ -0,0 +1,13 @@
+import "./style.css"
+
+function IndexNewtab() {
+  return (
+    <div>
+      <h1 className="text-red-500 text-6xl">
+        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
+      </h1>
+    </div>
+  )
+}
+
+export default IndexNewtab
